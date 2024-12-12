f107e48dabd18a824f45a7846b58b652b86dc7ee
erikaCutugno <erikucciapop@gmail.com>
2024-12-12 18:21:29 +0100

    fix object

--- snack2/script.js
+++ snack2/script.js
@@ -30,8 +30,7 @@ for(let i = 0; i < soccer.length; i++){
     soccerSquad.point = getRndNumber(1,114)
     soccerSquad.foul = getRndNumber(1,20) 
     const newSoccerSquad= {
-        newSquad:"",
-        newFoul:"",
+
     };
     newSoccerSquad.newSquad = soccerSquad.squad
     newSoccerSquad.newFoul = soccerSquad.foul
